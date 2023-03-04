000fdfedd9f3faf01f2ee617ed38283ccc886ad7
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2023-03-04 18:43:39 +0200

    Setting header title to a new font

--- src/renderer/src/components/Container.vue
+++ src/renderer/src/components/Container.vue
@@ -9,21 +9,10 @@ const store = useRecordingStatus()
   <div v-if="store.recording" class="bg-black pb-28">
     <Message />
     <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
   </div>
 
   <div v-else class="flex flex-col flex-grow justify-center items-center">
-    <h1
-      class="text-white text-9xl font-serif"
-      :class="{ animate__animated: store.recording, animate__fadeOutDown: store.recording }"
-    >
-      Welcome!
-    </h1>
+    <h1 class="text-white text-9xl font-serif">Welcome!</h1>
     <p class="font-serif">Press play button to start recording.</p>
   </div>
 </template>
--- src/renderer/src/components/Header.vue
+++ src/renderer/src/components/Header.vue
@@ -10,7 +10,7 @@ const { status } = storeToRefs(store)
   <!-- container -->
   <div class="flex justify-between p-4">
     <!-- title -->
-    <div class="text-white text-4xl font-poppins">Audio Transcriber</div>
+    <div class="text-white text-4xl font-semibold font-serif">Audio Transcriber</div>
 
     <!-- websocket connection status -->
     <div class="flex items-center">
