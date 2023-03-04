0e4aa502138aaa456970a27dc48ec01880256b30
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2023-03-04 02:16:18 +0200

    Display time in audio controls

--- src/renderer/src/components/AudioControls.vue
+++ src/renderer/src/components/AudioControls.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { useStopwatch } from 'vue-timer-hook'
+
+const stopwatch = useStopwatch(0, false)
+</script>
+
 <template>
-  <div class="flex justify-center font-serif">Audio Controls</div>
+  <div class="flex justify-center">
+    <div>
+      <div>
+        <span>{{ stopwatch.hours }}</span
+        >:<span>{{ stopwatch.minutes }}</span
+        >:<span>{{ stopwatch.seconds }}</span>
+      </div>
+      <p>{{ stopwatch.isRunning ? 'Running' : 'Not running' }}</p>
+      <button @click="stopwatch.start()">Start</button>
+      <button @click="stopwatch.pause()">Pause</button>
+      <button @click="stopwatch.reset()">Reset</button>
+    </div>
+  </div>
 </template>
