dc4c0a92f8a91f1d9468d73626432fb8bab21e47
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2023-03-04 14:22:04 +0200

    Use oh-vue-icons package to display play pause buttons in vue

--- src/renderer/src/components/AudioControls.vue
+++ src/renderer/src/components/AudioControls.vue
@@ -2,18 +2,28 @@
 import { useStopwatch } from 'vue-timer-hook'
 
 const stopwatch = useStopwatch(0, false)
+
+const changeStopwatchAction = (): void => {
+  if (stopwatch.isRunning.value) {
+    stopwatch.reset()
+    stopwatch.pause()
+  } else {
+    stopwatch.start()
+  }
+}
 </script>
 
 <template>
-  <div class="flex justify-center items-center h-20">
-    <div>
-      <span>{{ stopwatch.hours }}</span
-      >:<span>{{ stopwatch.minutes }}</span
-      >:<span>{{ stopwatch.seconds }}</span>
+  <div class="flex flex-col justify-center items-center h-32">
+    <button
+      class="w-16 h-16 rounded-full bg-red-900 mb-4 focus:outline-none flex justify-center items-center"
+      @click="changeStopwatchAction"
+    >
+      <v-icon v-if="stopwatch.isRunning.value" name="co-media-pause" scale="2" />
+      <v-icon v-else name="co-media-play" scale="2" />
+    </button>
+    <div class="badge bg-red-900 w-32">
+      {{ stopwatch.hours }} : {{ stopwatch.minutes }} : {{ stopwatch.seconds }}
     </div>
-    <p>{{ stopwatch.isRunning ? 'Running' : 'Not running' }}</p>
-    <button @click="stopwatch.start()">Start</button>
-    <button @click="stopwatch.pause()">Pause</button>
-    <button @click="stopwatch.reset()">Reset</button>
   </div>
 </template>
--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import 'animate.css'
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { CoMediaPlay, CoMediaPause } from 'oh-vue-icons/icons'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+addIcons(CoMediaPlay, CoMediaPause)
+
+createApp(App).component('v-icon', OhVueIcon).mount('#app')
